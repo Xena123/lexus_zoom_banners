726c478d7359d77acc1fe798ddf6cbe2a6c244ae
Cheryl Casteling <cheryl.casteling@gmail.com>
2020-03-12 11:11:01 +0000

    Initial commit

---
+++ Leaderboard/animations.js
@@ -0,0 +1,67 @@
+var fadeOutSpeed = 500;
+var fadeInSpeed = 500;
+var frameDuration = 4000;
+
+// Frame 1
+function startAnimation() {
+  $('#text-1,#logo,#centre-name').animate({ 
+    opacity: 1
+  },fadeInSpeed, function(){
+    setTimeout(frame2,frameDuration);
+  });
+
+  $( "#bg-img" ).addClass('zoomIn');
+
+}
+
+//Frame 2
+function frame2(){
+  $('#text-1').animate({ 
+    opacity: 0
+  },fadeOutSpeed, function(){
+    setTimeout(frame3,fadeOutSpeed);
+  });
+}
+
+//Frame 3
+function frame3(){
+  $('#text-2,#smallPrint').animate({ 
+    opacity: 1
+  },fadeInSpeed, function(){
+    setTimeout(frame4,frameDuration);
+  });
+}
+
+//Frame 4
+function frame4(){
+  $('#text-2,#smallPrint').animate({ 
+    opacity: 0
+  },fadeOutSpeed, function(){
+    setTimeout(frame5,fadeOutSpeed);
+  });
+}
+
+// Frame 5
+function frame5(){
+  $('#text-3').animate({ 
+    opacity: 1
+  },fadeInSpeed, function(){
+    setTimeout(frame6,frameDuration);
+  });
+}
+
+// Frame 6
+function frame6(){
+  $('#text-3').animate({ 
+    opacity: 0
+  },fadeOutSpeed, function(){
+    setTimeout(frame7,fadeOutSpeed);
+  });
+}
+
+// Frame 9
+function frame7(){
+  $('#text-4, #text-4--cta').animate({ 
+    opacity: 1
+  },fadeInSpeed);
+}
---
+++ MPU/setup.js
@@ -0,0 +1,50 @@
+var ctaExit;
+var centreName;
+
+if (!Enabler.isInitialized()) {
+  Enabler.addEventListener(studio.events.StudioEvent.INIT, enablerInitialized);
+} else {
+  enablerInitialized();
+}
+
+function enablerInitialized() {
+  document.getElementById('overlay').addEventListener('click', bgExitHandler, false);
+  if (!Enabler.isPageLoaded()) {
+    Enabler.addEventListener(studio.events.StudioEvent.PAGE_LOADED, init);
+  } else {
+    init();
+  }
+}
+
+function init() {
+  
+    Enabler.setProfileId(10014885);
+    var devDynamicContent = {};
+
+    devDynamicContent.Lexus_Business_WLT_Sheet1 = [{}];
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0]._id = 0;
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].ID = 1;
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].Reporting_Label = "BUSCT";
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].Lexus_Name = "Lexus Belfast";
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].Centre_Name = "Belfast";
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].CTA = {};
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].CTA.Url = "";
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].CTA_Win = {};
+    devDynamicContent.Lexus_Business_WLT_Sheet1[0].CTA_Win.Url = "https://www.lexus.co.uk/centres/belfast/latest-offers/item?docid=1416286&utm_source=display&utm_medium=cpm&utm_campaign=ebaon";
+    Enabler.setDevDynamicContent(devDynamicContent);
+
+    ctaExit = dynamicContent.Lexus_Business_WLT_Sheet1[0].CTA.Url;
+
+    centreName = dynamicContent.Lexus_Business_WLT_Sheet1[0].Centre_Name;
+    lexusCentreName = dynamicContent.Lexus_Business_WLT_Sheet1[0].Lexus_Name;
+    document.getElementById("dynamic-retailer-name").innerHTML = centreName;
+    document.getElementById("dynamic-retailer-name--two").innerHTML = lexusCentreName;
+
+    startAnimation();
+}
+
+var bgExitHandler = function(e) {
+  e.preventDefault();
+  Enabler.exitOverride('CTA', ctaExit);
+  return false;
+}
